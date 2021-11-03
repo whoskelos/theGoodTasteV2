821d9ed17289a1577c5c43de45b27abe15645a82
Kelvin Guerrero <kelvinguerrero2199@gmail.com>
2021-11-03 01:05:57 +0100

    editando pagina de resenas, anado botones, edito boton anadir

---
+++ js/funciones.js
@@ -0,0 +1,32 @@
+//function para anadir resena
+// function addResena() {
+//     let contenedor = document.createElement("div");
+//     contenedor.classList.add("modal-dialog","modal-dialog-centered");
+//     document.body.appendChild(contenedor);
+// }
+
+//function para editar resena
+// function editResena() {
+    
+// }
+
+//function para eliminar resena
+// function delResena() {
+    
+// }
+
+
+
+
+// onload = function() {
+//     cargarBotones();    
+// }
+// //function para anadir los event listeners los botones
+// function cargarBotones() {
+//     let btnAdd = document.getElementById("btn-add");
+//     // let btnEdit = document.getElementById("btn-edit");
+//     // let btnDel = document.getElementById("btn-del");
+//     // btnAdd.addEventListener("click", addResena);
+//     // btnEdit.addEventListener("click", editResena);
+//     // btnDel.addEventListener("click", delResena);
+// }
