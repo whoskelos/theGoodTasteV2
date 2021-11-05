0f708bb62406d6898d3916bdd51f4ea47862b0ca
whoskelos <Kelvin@MacBook-Pro-de-Kelvin-Guerrero.local>
2021-11-05 10:21:02 +0100

    añador directorio datos, para cargar los datos mediante JSON

--- js/funciones.js
+++ js/funciones.js
@@ -1,17 +1,24 @@
-
 $(document).ready(function () {
     //Boton añadir tarjeta
-    $('btnGuardar').click(function(){
-        $('hidden').style('display:visible');
+    $('#btnGuardar').click(function () {
+        var nombreRestaurante = $('#nombreRestaurante').val();
+        var ubicacion = $('#ubicacion').val();
+        var tipo = $('#tipo').val();
+        var comentario = $('#floatingTextarea').val();
+        var imagen = $('#formFile').val();
+        $('#card-1').removeClass('hidden');
+        $('.card-title').html(nombreRestaurante);
+        $('.card-text').html(comentario);
+
+        var contador = $('.card').length + 1;
+        var bloque = '<div id="card-' + contador + '"class="card galeria">texto prueba</div>';
+        var img = $('<img class="card-img-top">');
+        img.attr('src', '');
+        img.appendTo('#card-2');
+        $('#tarjetas').append(bloque);
+        var cardBody = $('<div class="card-body"></div>');
+        $(bloque).append(cardBody);
     });
-});
-
-//boton guardar
-
-// var btnGuardar = document.getElementById("btnGuardar");
-
-$('btnGuardar').click(function(){
-    $('hidden').style('display:visible');
 });
 
 
