bce359c431fa90d23da6145f2ff6926ef6256fcc
whoskelos <kelvinguerrero2199@gmail.com>
2021-11-22 14:30:14 +0100

    registro usuarios funcionando

--- js/registrarUsuarios.js
+++ js/registrarUsuarios.js
@@ -6,18 +6,19 @@ onload = function () {
 
 var listaUsuarios = [];
 
-
 function cargarAdmins() {
     if (localStorage.getItem("usuarios") == null) {
         $.get("../datos/administradores.txt", {}, (respuesta) => {
-            localStorage.setItem("usuarios",respuesta);
-            listaUsuarios = JSON.parse(
-                localStorage.getItem("usuarios")
-            );
+            localStorage.setItem("usuarios", respuesta);
+            listaUsuarios = JSON.parse(localStorage.getItem("usuarios"));
         });
     } else {
         listaUsuarios = JSON.parse(localStorage.getItem("usuarios"));
     }
+}
+
+function refrescar() {
+    location.reload();
 }
 
 function recogerDatos() {
@@ -27,29 +28,34 @@ function recogerDatos() {
     let usuario = document.getElementById("usuarioNuevo");
     let pass1 = document.getElementById("password1");
     let pass2 = document.getElementById("password2");
-    if (usuarioRepetido(usuario)){
+    if (usuarioRepetido(usuario)) {
         $(".mensajeError").empty();
         $(".mensajeError").append("<span>Este usuario ya existe</span>");
+        setTimeout(refrescar, 5000);
     } else {
         if (pass1.value == pass2.value) {
-            registrarUsuario(nombre, email, usuario, pass1, pass2);
-            $(".mensajeConfirmacion").empty();
+            registrarUsuario(nombreCompleto, email, usuario, pass1, pass2);
+            $(".mensajeError").empty();
             $(".mensajeConfirmacion").append(
-                "<span>Usuario creado correctamente</span>");
+                "<span>Usuario creado correctamente</span>"
+            );
+            setTimeout(refrescar, 2000);
         } else {
             $(".mensajeError").empty();
-            $(".mensajeError").append("<span>Las contraseñas no coinciden</span>");    
+            $(".mensajeError").append(
+                "<span>Las contraseñas no coinciden</span>"
+            );
         }
     }
 }
 
-    
-
- //EN ESTA FUNCION COMPROBAMOS SI EL USUARIO QUE SE INTENTA CREAR YA EXISTE
+//EN ESTA FUNCION COMPROBAMOS SI EL USUARIO QUE SE INTENTA CREAR YA EXISTE
 function usuarioRepetido(usuario, nombre, email, pass1, pass2) {
     console.log("He entrado a la funcion usuarioRepetido");
-    if (listaUsuarios.findIndex(ele => ele.user.includes(usuario.value))!=-1){
-       return true;
+    if (
+        listaUsuarios.findIndex((ele) => ele.user.includes(usuario.value)) != -1
+    ) {
+        return true;
     } else {
         return false;
     }
@@ -61,19 +67,19 @@ function usuarioRepetido(usuario, nombre, email, pass1, pass2) {
 // }
 
 function registrarUsuario(nombre, email, user, pass1, pass2) {
-        var listaUsuarios=[];
-        if (localStorage.getItem("usuarios") != null) {
-            listaUsuarios = JSON.parse(localStorage.getItem("usuarios"));
-        }
-            //AQUI CREO EL NUEVO USUARIO EN FORMA DE OBJETO Y LE AÑADO LOS VALORES QUE RECIBE DEL INPUT
-            let _usuarios = new usuario(
-                nombre.value,
-                email.value,
-                user.value,
-                pass1.value
-            );
-            //CARGO LOS USUARIOS EN UN ARRAY
-            listaUsuarios.push(_usuarios);
-            //GUARDO EN EL LOCALSTORAGE EL USUARIO NUEVO
-            localStorage.setItem("usuarios", JSON.stringify(listaUsuarios));        
+    var listaUsuarios = [];
+    if (localStorage.getItem("usuarios") != null) {
+        listaUsuarios = JSON.parse(localStorage.getItem("usuarios"));
+    }
+    //AQUI CREO EL NUEVO USUARIO EN FORMA DE OBJETO Y LE AÑADO LOS VALORES QUE RECIBE DEL INPUT
+    let _usuarios = new usuario(
+        nombre.value,
+        email.value,
+        user.value,
+        pass1.value
+    );
+    //CARGO LOS USUARIOS EN UN ARRAY
+    listaUsuarios.push(_usuarios);
+    //GUARDO EN EL LOCALSTORAGE EL USUARIO NUEVO
+    localStorage.setItem("usuarios", JSON.stringify(listaUsuarios));
 }
